04306d55822407f8f4957878ca13e774d82c6c55
maximus-sunshine <maxsun96@gmail.com>
2021-04-20 14:24:07 -0700

    changed weather.py to use ladybug-pandas backend

--- weather.py
+++ weather.py
@@ -131,7 +131,7 @@ uploaded_file = st.file_uploader("Upload a .epw file")
 
 if st.button('See example'):
     # epw_df = read_epw(epw_path + random.choice(os.listdir(epw_path)))  # pick a random example file
-    epw_path = '/app/hvaclib/Weather Files/CZ06RV2.epw'
+    epw_path = '/app/hvaclib/Weather Files/CZ06RV2'
     epw_df = read_epw(epw_path)
     fig = plot_epw(epw_df)
 
