99992a6b3b2f59a7a9277c840383dbdf3b7ae05e
maximus-sunshine <maxsun96@gmail.com>
2021-04-20 14:10:51 -0700

    changed weather.py to use ladybug-pandas backend

--- test.py
+++ test.py
@@ -90,10 +90,10 @@ def plot_epw(epw_df):
 
     # create traces for all columns vs. timestamps
     for i in range(epw_df.shape[1] - 1):
-        fig.add_trace(go.Scatter(x=epw_df.index, y=epw_df.iloc[:, i + 1],
+        fig.add_trace(go.Scatter(x=epw_df.index, y=epw_df.iloc[:, i+1],
                                  visible='legendonly',
                                  mode='lines',
-                                 name=epw_df.columns[i + 1]))
+                                 name=epw_df.columns[i+1]))
 
     # layout configuration
     fig.update_layout(showlegend=True)  # force the legend for single-trace plots
@@ -107,81 +107,9 @@ def plot_epw(epw_df):
 # SCRIPT
 
 # do some housekeeping and create some variables
-st.set_page_config(layout="wide")
-epw_path = './Weather Files/'
-epw_df = read_epw(epw_path)
-fig = go.Figure()
 
-# use triple quotes instead of st.write() for multiline printing using Markdown syntax
-# (https://www.markdownguide.org/cheat-sheet/)
-"""`Welcome to the party! This is an experimental app. If you run 
-into any bugs/errors or have suggestions for additional features/functionality, please use the "Report a bug with 
-this app" tool in the drop down menu in the top right corner of this page. Thanks for playing!` 
 
-# Weather Data Visualization App
-
-Quickly visualize weather data to gain engineering insights and make nice-looking graphs to include in 
-reports/presentations.
-
-This app is compatible with weather data files saved in the [.epw](https://energyplus.net/weather/sources) EnergyPlus format.
-
-To get started, upload a .epw file - or click "See example"."""
-
-uploaded_file = st.file_uploader("Upload a .epw file")
-
-if st.button('See example'):
-    epw_df = read_epw(epw_path + random.choice(os.listdir(epw_path)))  # pick a random example file
-    fig = plot_epw(epw_df)
-
-    """
-    ### Raw Data (example):
-
-    Uploaded file should be in .epw format
-
-    Click "See example" again to see a different example.
-    """
-
-    # this line displays the .csv file in table format, with the index column suppressed to avoid confusion
-    st.dataframe(epw_df.assign(drop_index='').set_index('drop_index'))
-
-    """
-    ### Point Trend Graph (example):
-
-    Click on point names in the legend to make them visible.
-
-    Pan and zoom with your mouse to get a closer look at the data. Double click inside the graph to reset the axes.
-
-    You can download this graph as a .png by clicking the camera icon in the plot figure menu.
-    """
-
-    st.plotly_chart(fig, use_container_width=True)
-
-if uploaded_file is not None:
-    epw_df = read_epw(uploaded_file)
-
-    fig = plot_epw(epw_df)
-
-    """
-    ### Raw Data:
-
-    Uploaded file should be in .epw format
-
-    Click "See example" again to see a different example, or upload a different file.
-    """
-
-    # this line displays the .csv file in table format, with the index column suppressed to avoid confusion
-    st.dataframe(epw_df.assign(drop_index='').set_index('drop_index'))
-
-    """
-    ### Point Trend Graph:
-
-    Click on point names in the legend to make them visible.
-
-    Pan and zoom with your mouse to get a closer look at the data. Double click inside the graph to reset the axes.
-
-    You can download this graph as a .png by clicking the camera icon in the plot figure menu.
-    """
-
-    st.plotly_chart(fig, use_container_width=True)
+fig = plot_epw(epw_df)
+fig.show()
 
 
